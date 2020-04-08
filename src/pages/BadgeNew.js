47d69df207888329155e66e7e9de426279ec6326
Jhoss <kuro.rc10@gmail.com>
2020-04-08 17:23:23 -0500

    Creando el header con el nav con sus estilos

--- src/pages/BadgeNew.js
+++ src/pages/BadgeNew.js
@@ -1,9 +1,34 @@
 import React from 'react'
 
+import './styles/BadgeNew.css'
+import header from '../images/badge-header.svg'
+import Navbar from '../components/Navbar'
+import Badge from '../components/Badge';
+
 class BadgeNew extends React.Component {
     render() {
         return (
-            <h1>Badge New</h1>
+            <div>
+                <Navbar/>
+                <div className="BadgeNew__hero">
+                    <img className="img-fluid" src={header} alt="Logo"/>
+                </div>
+
+                <div className="container">
+                    <div className="row">
+                        <div className="col">
+                            <Badge
+                                firstName="José" 
+                                lastName="Robles" 
+                                jobTitle="Kickass senior dev" 
+                                avatarUrl="https://i.imgur.com/HTBd54R.jpg"
+                                twitter="jossrc10"
+                            />
+                        </div>
+                    </div>
+                </div>
+
+            </div>
         );
     };
 }
