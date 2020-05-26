70d9358558638bf3b11260ec1fb08799bede008c
Jhoss <kuro.rc10@gmail.com>
2020-05-26 12:26:22 -0500

    Separando la lógica en UI Components y Container Components

--- src/components/App.routes.js
+++ src/components/App.routes.js
@@ -10,7 +10,7 @@ import NotFound from "../pages/NotFound"
 import Badges from "../pages/Badges";
 import BadgeNew from "../pages/BadgeNew";
 import BadgeEdit from "../pages/BadgeEdit";
-import BadgeDetails from "../pages/BadgeDetails";
+import BadgeDetailsContainer from "../pages/BadgeDetailsContainer";
 
 function App() {
   return (
@@ -19,7 +19,7 @@ function App() {
         <Switch>
           <Route exact path='/badges' component={Badges} />
           <Route exact path='/badges/new' component={BadgeNew} />
-          <Route exact path='/badges/:badgeId' component={BadgeDetails} />
+          <Route exact path='/badges/:badgeId' component={BadgeDetailsContainer} />
           <Route exact path='/badges/:badgeId/edit' component={BadgeEdit} />
           <Route component={NotFound} />
         </Switch>
---
+++ src/pages/BadgeDetailsContainer.js
@@ -0,0 +1,51 @@
+import React from "react";
+
+import "./styles/BadgeDetails.css";
+import PageLoading from '../components/PageLoading';
+import PageError from '../components/PageError';
+
+import BadgeDetails from '../pages/BadgeDetails';
+
+import api from '../api';
+
+class BadgeDetailsContainer extends React.Component {
+
+  state = {
+    loading: true,
+    error: null,
+    data: undefined
+  };
+
+  componentDidMount() {
+    this.fetchData();
+  }
+
+  fetchData= async () => {
+    this.setState({loading: true, error: null});
+    try {
+      const data = await api.badges.read(
+        this.props.match.params.badgeId
+      )
+      this.setState({loading: false, data})
+    } catch (error) {
+      this.setState({loading: false, error})
+    }
+  }
+
+  render() {
+
+    if ( this.state.loading ) {
+      return <PageLoading/>;
+    }
+
+    if ( this.state.error ) {
+      return <PageError error={this.state.error} />
+    }
+
+    return (
+      <BadgeDetails badge={this.state.data}/>
+    );
+  }
+}
+
+export default BadgeDetailsContainer;
